aae8e9f0ff7cc3e1af3fa2a41a9034a5aee404ce
elzmkavakli <elzmkavakli@gmail.com>
2021-07-05 19:55:30 +0000

    Son Islem yapildi

--- mehmet.py
+++ mehmet.py
@@ -1,7 +1,8 @@
 # Fill in this file with the code from parsing JSON exercise
 import json
-dosya=open("myfile.json","r")
+dosya=open("mehmet.json","r")
 json_dosya=json.load(dosya)
 print("API-KEY :",json_dosya["Ad"])
 print("API-KEY :",json_dosya["Soyad"])
 print("API KEY:",json_dosya["access_token"])
+print("API KEY:",json_dosya["kimlik"])
